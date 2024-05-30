20a83f14d7589a0f0ad6bb1db23d76571deb455b
ardihp <ardihp8@gmail.com>
2024-05-31 00:54:07 +0700

    feat(hooks): use-users

---
+++ src/lib/hooks/use-users.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+
+export function useUsers() {
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  async function getListUser() {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+
+      if (response?.status === 200) {
+        setLoading(false);
+        return response?.data;
+      } else {
+        throw response;
+      }
+    } catch (err) {
+      setLoading(false);
+      toast.error("Gagal mengambil data, silahkan coba lagi");
+    }
+  }
+
+  return {
+    isLoading,
+    getListUser,
+  };
+}
