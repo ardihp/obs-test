cd95ad7a1a3c8a4f6e0e7c9335c552f2b2446003
ardihp <ardihp8@gmail.com>
2024-05-31 13:27:05 +0700

    fix(types): user

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,13 +1,11 @@
 export interface UserCompany {
   name: string;
   catchPhrase: string;
-  bs: string;
 }
 
 export interface User {
   id: number;
   name: string;
   username: string;
-  email: string;
   company: UserCompany;
 }
