b13c34a70c04bea84b7e5e70c4bc568b11ebd895
ardihp <ardihp8@gmail.com>
2024-05-31 13:26:42 +0700

    feat(redux): userSlice function

--- src/lib/redux/slice/userSlice.ts
+++ src/lib/redux/slice/userSlice.ts
@@ -3,25 +3,56 @@ import { User } from "types/user";
 
 interface UserState {
   list: User[];
+  detail: User;
 }
 
 const initialState: UserState = {
   list: [],
+  detail: {
+    id: 0,
+    name: "",
+    username: "",
+    company: {
+      name: "",
+      catchPhrase: "",
+    },
+  },
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser: (state, action: PayloadAction<User[]>) => {
+    setListUser: (state, action: PayloadAction<User[]>) => {
       state.list = action.payload;
+    },
+    setDetailUser: (state, action: PayloadAction<User>) => {
+      state.detail = action.payload;
     },
     deleteUser: (state, action: PayloadAction<number>) => {
       state.list = state.list?.filter((user) => user?.id !== action.payload);
     },
+    updateUser: (state, action: PayloadAction<User>) => {
+      state.list = state.list?.map((user) => {
+        if (user?.id === action.payload?.id) {
+          return action.payload;
+        } else {
+          return user;
+        }
+      });
+    },
+    createUser: (state, action: PayloadAction<User>) => {
+      state.list = [action.payload, ...state.list];
+    },
   },
 });
 
-export const { setUser, deleteUser } = userSlice.actions;
+export const {
+  setListUser,
+  setDetailUser,
+  deleteUser,
+  updateUser,
+  createUser,
+} = userSlice.actions;
 
 export default userSlice.reducer;
